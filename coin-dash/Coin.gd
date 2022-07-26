3bec754da0b175b7b80153a9c157b0b0510bcf79
Chris Pritchard <chris@grislygrotto.nz>
2022-07-27 08:29:56 +1200

    started coin-dash, up to coin scene

---
+++ coin-dash/Coin.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+
+func pickup():
+	queue_free()
---
+++ coin-dash/Player.gd
@@ -0,0 +1,54 @@
+extends Area2D
+
+signal pickup
+signal hurt
+
+export (int) var speed = 350
+
+var velocity = Vector2()
+var screen_size = Vector2(480, 720)
+
+func start(pos):
+	set_process(true)
+	position = pos
+	$AnimatedSprite.animation = "idle"
+
+func get_input():
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+func _process(delta):
+	get_input()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	if velocity.length() > 0:
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite.animation = "idle"
+
+func die():
+	$AnimatedSprite.animation = "hurt"
+	set_process(false)
+
+func _on_Player_area_entered(area):
+	if area.is_in_group("coins"):
+		area.pickup()
+		emit_signal("pickup")
+	if area.is_in_group("obstacles"):
+		emit_signal("hurt")
+		die()
