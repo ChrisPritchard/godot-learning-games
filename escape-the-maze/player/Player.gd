1b9965083d6d616e6f99ce4c2c249213afa9e761
Chris Pritchard <chris@grislygrotto.nz>
2022-07-28 07:35:58 +1200

    players and enemy inherited scenes

---
+++ escape-the-maze/player/Player.gd
@@ -0,0 +1,23 @@
+extends "res://character/Character.gd"
+
+signal moved
+signal dead
+signal has_key
+signal win
+
+func _process(delta):
+	if can_move:
+		for dir in moves.keys():
+			if Input.is_action_pressed(dir):
+				if move(dir):
+					emit_signal("moved")
+
+func _on_Player_area_entered(area):
+	if area.is_in_group("enemies"):
+		emit_signal("dead")
+	if area.has_method("pickup"):
+		area.pickup()
+	if area.type == "key_red":
+		emit_signal("has_key")
+	if area.type == "star":
+		emit_signal("win")
