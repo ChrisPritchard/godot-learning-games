efb96330584400cfa09b1ea06c711cc0dc20412e
Chris Pritchard <chris@grislygrotto.nz>
2022-07-31 11:50:01 +1200

    enemy suffering damage and shooting

--- space-rocks/bullet/EnemyBullet.gd
+++ space-rocks/bullet/EnemyBullet.gd
@@ -15,11 +15,4 @@ func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
 func _on_Bullet_body_entered(body):
-	if body.is_in_group("rocks"):
-		body.explode()
-		queue_free()
-
-func _on_Bullet_area_entered(area):
-	if area.is_in_group("enemies"):
-		area.take_damage(1)
 	queue_free()
--- space-rocks/enemy/Enemy.gd
+++ space-rocks/enemy/Enemy.gd
@@ -19,12 +19,43 @@ func _ready():
 func _process(delta):
 	follow.offset += speed * delta
 	position = follow.global_position
-	if follow.unit_offset > 1:
+	if follow.unit_offset >= 1:
 		queue_free()
 
 func _on_Explosion_animation_finished():
 	queue_free()
 
 func _on_GunTimer_timeout():
-	pass # Replace with function body.
+	shoot_pulse(3, 0.15)
+	
+func shoot_pulse(n, delay):
+	for _i in range(n):
+		shoot()
+		yield(get_tree().create_timer(delay), "timeout")
 
+func shoot():
+	var dir = target.global_position - global_position
+	dir = dir.rotated(rand_range(-0.1, 0.1)).angle()
+	emit_signal("shoot", Bullet, global_position, dir)
+
+func take_damage(amount):
+	health -= amount
+	$Sprite/AnimationPlayer.play("flash")
+	if health <= 0:
+		explode()
+	yield($Sprite/AnimationPlayer, "animation_finished")
+	$Sprite/AnimationPlayer.play("rotate")
+
+func explode():
+	speed = 0
+	$GunTimer.stop()
+	$CollisionShape2D.set_deferred("disabled", true)
+	$Sprite.hide()
+	$Explosion.show()
+	$Explosion.play()
+	$ExplodeSound.play()
+
+func _on_Enemy_body_entered(body):
+	if body.name == "Player":
+		pass
+	explode()
