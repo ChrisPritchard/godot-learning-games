a8312f511480c43dcebc543f4758556215158b5b
Christopher (mite) <chris@grislygrotto.nz>
2024-06-12 15:43:32 +1200

    reorganised for new learning

---
+++ dodge-the-creeps/scripts/player.gd
@@ -0,0 +1,69 @@
+extends Area2D
+
+@export var speed = 400
+var screen_size
+var direction = 0
+
+signal hit
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("hello world")
+	screen_size = get_viewport_rect().size
+	hide()
+	
+func start(pos):
+	position = pos
+	$CollisionShape2D.disabled = false
+	show()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = update_direction()
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		update_animation(velocity)
+	else:
+		$AnimatedSprite2D.stop()
+		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func update_direction():
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		direction = 3
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+		velocity.x += 1
+	if Input.is_action_pressed("ui_up"):
+		direction = 0
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction = 2
+		velocity.y += 1
+	return velocity
+
+func update_animation(velocity):
+	var animation = "up"
+	
+	if direction == 1 or direction == 3:
+		animation = "walk"
+	if direction == 0 or direction == 1: 
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.flip_v = false
+	elif direction == 2:
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.flip_v = true
+	else:
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.flip_v = false
+		
+	$AnimatedSprite2D.play(animation)
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
