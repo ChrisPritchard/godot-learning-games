a5c989f472bdd9916846c48c3edc87b9b5853434
= <=>
2024-06-13 16:59:19 +1200

    work on implementing player rust class

--- dodge-the-creeps-rust/rust/src/player.rs
+++ dodge-the-creeps-rust/rust/src/player.rs
@@ -1,12 +1,62 @@
-use godot::{engine::IArea2D, prelude::*};
+use godot::{engine::{AnimatedSprite2D, Area2D, IArea2D}, prelude::*};
+
+enum Direction { Left, Right, Up, Down }
 
 #[derive(GodotClass)]
-#[class(base=Area2D)]
-struct Player {}
+#[class(base=Area2D,no_init)]
+struct Player {
+    speed: f32,
+    screen_size: Vector2,
+    direction: Direction,
+
+    base: Base<Area2D>
+}
 
 #[godot_api]
 impl IArea2D for Player {
-    fn init(_base: Base <Self::Base>) -> Self {
-        Self {}
+    fn ready(&mut self) {
+        self.speed = 400.;
+        self.screen_size = self.base().get_viewport_rect().size;
+        self.direction = Direction::Right;
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        godot_print!("Hello, world!");
+
+        let mut velocity = Vector2::ZERO;
+        let input = Input::singleton();
+
+        if input.is_action_pressed("ui_left".into()) {
+            velocity.x -= 1.;
+            self.direction = Direction::Left;
+        }
+
+        if input.is_action_pressed("ui_right".into()) {
+            velocity.x += 1.;
+            self.direction = Direction::Right;
+        }
+
+        if input.is_action_pressed("ui_up".into()) {
+            velocity.y -= 1.;
+            self.direction = Direction::Up;
+        }
+
+        if input.is_action_pressed("ui_down".into()) {
+            velocity.y += 1.;
+            self.direction = Direction::Down;
+        }
+
+        let mut animation_player = self.base().get_node_as::<AnimatedSprite2D>("AnimatedSprite2D");
+
+        if velocity.length() > 0. {
+            velocity = velocity.normalized() * self.speed;
+            let screen_size = self.screen_size;
+            let mut node = self.base_mut();
+            let position = node.get_position() + velocity * delta as f32;
+            node.set_position(position + position.clamp(Vector2::ZERO, screen_size));
+            animation_player.play();
+        } else {
+            animation_player.stop()
+        }
     }
 }
